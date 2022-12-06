8793d536a7a36190979ab2b9759627807090b9ec
ndtran <nox.tran@gmail.com>
2022-12-06 10:16:16 +0100

    AoC 2022 day6

--- 2022/day6.js
+++ 2022/day6.js
@@ -1,32 +1,31 @@
 const fs = require('fs');
-const { text } = require('stream/consumers');
-
 try {
     const data = fs.readFileSync('day6.txt', 'UTF-8')
     const lines = data.split("\n")
 
-    let ans = 0
-    // let length = 4 // Part1
-    let length = 14 // Part2
-
-    
-    for(let i=0; i<data.length-length; i++) {
-        let word = data.substring(i, i+length)
-        // console.log(word, i)
-    
-        let tmp = ""
-        for(let j=0; j<word.length; j++) {
-            // console.log(tmp.indexOf(word[j]) != -1, tmp, word[j])
-            if (tmp.indexOf(word[j]) != -1) break
-            tmp += word[j]
-        }
-        if (tmp.length == length) {
-            ans = i+length
+    // Part1
+    let ans1 = 0
+    let lengthP1 = 4
+    for(let i=0; i<data.length; i++) {
+        if ((new Set(data.substring(i, i+lengthP1).split(''))).size === lengthP1) {
+            ans1 = i + lengthP1
             break
         }
     }
 
-    console.log("How many characters need to be processed before the first start-of-packet marker is detected? " + ans)
+    // Part2
+    let ans2 = 0
+    let lengthP2 = 14 
+    for(let i=0; i<data.length; i++) {
+        if ((new Set(data.substring(i, i+lengthP2).split(''))).size === lengthP2) {
+            ans2 = i + lengthP2
+            break
+        }
+    }
+
+    console.log("How many characters need to be processed before the first start-of-packet marker is detected? ")
+    console.log("Part1: " + ans1)
+    console.log("Part2: " + ans2)
 } catch (err) {
   console.error(err)
 }
