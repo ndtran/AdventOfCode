7bd52fcda855e552267257bc6a34e068787439aa
ndtran <nox.tran@gmail.com>
2022-12-18 22:38:12 +0100

    AoC 2022 day18

--- 2022/day18.js
+++ 2022/day18.js
@@ -35,18 +35,6 @@ try {
     // PART 2
 
     // TBD
-    // ans2 = adjacentCubes
-    //     .filter((i) => !cubes.includes(i))
-    //     .filter((c) => {
-    //         let [x, y, z] = c.split(',').map((i) => parseInt(i))
-    //         let n = 0
-    //         for (let i = 0; i < ac.length; i++) {
-    //             let adjacentCube = [x + ac[i][0], y + ac[i][1], z + ac[i][2]].join()
-    //             if (adjacentCubes.includes(adjacentCube)) n += 1
-    //             else if (cubes.includes(adjacentCube)) n += 1
-    //         }
-    //         return n !== 6
-    //     }).length
 
     // Answers
     console.log('Part1 - What is the surface area of your scanned lava droplet?', ans1)
